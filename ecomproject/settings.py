c1e1200380d0c121cb3d9707a900cf680420a0b1
binshanb <binshanb77@gmail.com>
2023-07-28 15:06:37 +0530

    twilio check

--- ecomproject/settings.py
+++ ecomproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'ut@66t*#*_d58&-v%wei3*0wv1%6^(1n0^_0hat#j9fk5b8%&j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '0.0.0.0']
+ALLOWED_HOSTS = [ '0.0.0.0','127.0.0.1']
 
 
 # Application definition
